b167292d00bdab283d48ca153c40f3b69231046f
JLongo25 <30610160+JLongo25@users.noreply.github.com>
2021-05-31 16:59:50 -0700

    Create synciq_running.py

---
+++ synciq_running.py
@@ -0,0 +1,48 @@
+
+#!/usr/bin/env python3
+
+import isilon_helper
+from datetime import datetime
+import sys
+from elasticsearch import helpers
+from es_connect import connect_elasticsearch
+
+if len(sys.argv) >= 2:
+    host = sys.argv[1]
+else:
+    sys.exit("Missing Isilon Host Information.  Exiting program.")
+
+cluster = sys.argv[1]
+#cluster = 'cluster1'
+
+job_list_url = f'https://{cluster}:8080/platform/3/sync/jobs'
+#report_list_url = f'https://{cluster}:8080/platform/4/sync/reports'
+
+job_list = isilon_helper.get_metric(job_list_url)
+#report_list = isilon_helper.get_metric(report_list_url)
+
+#collection_date = datetime.now().strftime('%Y%m%d%H%M%S')
+collection_date = datetime.now().strftime('%s')
+
+running_job = job_list['jobs']
+for item in running_job:
+    if item['error']:
+        success = False
+    else:
+        success = True
+
+    item.update({"timestamp": collection_date, "cluster": cluster, "success": success})
+
+"""
+filename = "repo/isilon_sycnip_running_" + cluster + "_" + collection_date
+with open(filename, 'a') as output:
+    output.write('collection_date, cluster\n')
+    output.write(f'{collection_date}, {cluster}\n')
+    output.write(f'\n')
+    output.write(f'{running_job}\n')
+#print(filename)
+"""
+
+es = connect_elasticsearch()
+
+resp = helpers.bulk(es, running_job, index='isilon_job_list')
